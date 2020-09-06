eab5d9dc0a7ef11a01bab9c566b201a834c1d9f3
LAPTOP-NJM31021\jhonatan <jhonatanvieira@live.com.ar>
2020-09-06 20:29:13 -0300

    Avances. La aplicación envía y recibe datos desde diferentes máquinas

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,13 +13,24 @@ MainWindow::MainWindow(QWidget *parent)
     this->setWindowTitle("UDP SOCKET CHAT");
 
     myUdpSocket = new QUdpSocket(this);
-    myUdpSocket->bind(QHostAddress::LocalHost, 7755);
+
+    //obtengo datos de mi host y me quedo con la dirección ip
+    QList <QHostAddress> listDir = QHostInfo::fromName(QHostInfo::localHostName()).addresses();
+       //for (int i = 0; i < list.size(); i++)
+          //qDebug() << list.at(i).toString();
+    qDebug() << listDir.at(1).toString();
+    //-------------------------
+    //Realizo en bindeo de la ip y el puerto del servidor.
+    //Para que funcionen en direfentes máquinas deben tener diferentes puertos
+    myUdpSocket->bind(QHostAddress(listDir.at(1).toString()), 7755);
 
     //se hace una conección con el socket y la señal listo para leer
     connect(myUdpSocket, SIGNAL(readyRead()),this,SLOT(readSocket()));
 
     //seteo el editor de texto plano para que sea de solo lectura.
     ui->receivedMessages->setReadOnly(true);
+    ui->receivedMessages->appendPlainText("Mi dirección IP es: "+ QHostAddress(listDir.at(1).toString()).toString());
+    ui->receivedMessages->appendPlainText("Mi puerto es: "+QString("%1").arg(7755));
     connect(ui->message,SIGNAL(returnPresse()),this,SLOT(on_sendButton_clicked()));
 
 
@@ -57,11 +68,6 @@ void MainWindow::readSocket()
 
 void MainWindow::on_connectButton_clicked()
 {
-    //------------
-    QList <QHostAddress> list = QHostInfo::fromName(QHostInfo::localHostName()).addresses();
-       for (int i = 0; i < list.size(); i++)
-          qDebug() << list.at(i).toString();
-    //-------------------------
    //hago la conexion
    if(ui->ipAddress->text().isEmpty()){
 
@@ -71,7 +77,6 @@ void MainWindow::on_connectButton_clicked()
          //se gurdan las ip y los puertos
          ipAddress = QHostAddress(ui->ipAddress->text());
          port = ui->port->value();
-         //myUdpSocket->bind(ipAddress, port);
          QMessageBox::information(this, "Chat", "Conectando...");
       }
 
